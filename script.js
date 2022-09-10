bc2ec0e39c7c18213d88dce2d02a2a7796aa8a8a
parsa <parsaes771385@gmail.com>
2022-09-10 18:37:11 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let firstNameInput = document.querySelector('.firstName-input')
+let lastNameInput = document.querySelector('.lastName-input')
+let emailInput = document.querySelector('.email-input')
+let passwordInput = document.querySelector('.password-input')
+let firstNameText = document.querySelector('.firstName-text')
+let lastNameText = document.querySelector('.lastName-text')
+let emailText = document.querySelector('.email-text')
+let passwordText = document.querySelector('.password-text')
+let button = document.querySelector('button')
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+function inputCheck(input,text,innerText){
+    if(input.value == ''){
+        input.classList.add('invalid')
+        input.nextElementSibling.src = 'images/icon-error.png'
+        text.innerText = innerText
+    }else{
+        input.classList.remove('invalid')
+        input.nextElementSibling.src = ''
+        text.innerText = ''
+    }
+}
+
+button.addEventListener('click',(e)=>{
+    e.preventDefault()   
+
+    //first name input
+    inputCheck(firstNameInput,firstNameText,'First Name cannot be empty')
+    
+    //last name input
+    inputCheck(lastNameInput,lastNameText,'Last Name cannot be empty')
+
+    //email input
+    if(emailInput.value.match(pattern)){
+        emailText.innerText = ''
+        emailInput.classList.remove('invalid')
+        emailInput.nextElementSibling.src = ''
+    }else{
+        emailInput.classList.add('invalid')
+        emailInput.nextElementSibling.src = 'images/icon-error.png'
+        emailText.innerText = 'looks like this in not an email'
+        if(emailInput.value == ''){
+            emailText.innerText = 'email cannot be empty'
+        }
+    }
+
+    //password input
+    inputCheck(passwordInput,passwordText,'Password cannot be empty')
+})
